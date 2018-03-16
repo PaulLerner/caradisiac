b73380437d58753203f2858f7e63b7118d410117
Unknown <paul.lerner@devinci.fr>
2018-03-16 15:24:15 +0100

    get all the models

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+const {getModels} = require('node-car-api');
+
+var fs = require('fs');
+const {getBrands} = require('node-car-api');
+
+async function test(){
+    const brands = await getBrands();
+    brands.forEach(async function(brand) {
+  
+        var models = await getModels(brand);
+        console.log(models)
+    var json = JSON.stringify(models);
+        fs.appendFileSync('./models.json', json, 'utf8', (err) => {
+	  if (err) throw err;
+
+	});
+        });
+}
+test();
